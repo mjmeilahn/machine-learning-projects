4ff0b8f4db2f22707851e05892327fb19ce9f9a5
mjmeilahn <mjmeilahn@gmail.com>
2019-09-16 18:31:34 -0700

    Finished Linear Regression lesson

---
+++ 2-simple-linear-regression/script.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Fit Linear Regression to Training Set
+# Feature Scaling not needed, import has this built-in
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Initial prediction of Test Set Results
+y_predict = regressor.predict(X_test)
+
+# Visualize actual Training Set learning
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualize actual Test Set predictions
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+
+
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Machine Learning Projects
 
-1. Data Preprocessing - TBD
+1. Data Preprocessing - Basics of importing data, split observations between test and training groups. Feature scaling applied to normalize model.
+
+2. Simple Linear Regression - TBD

66576729431b1fe0f8197e2793c9e2c05b5fff1e
mjmeilahn <mjmeilahn@gmail.com>
2021-06-14 11:14:10 -0700

    Update documentation

--- 1-data-preprocessing/4-final-script.py
+++ 1-data-preprocessing/4-final-script.py
@@ -1,6 +1,12 @@
 
 import pandas as pd
 
+# PURPOSE IS TO PREDICT PURCHASE RATES
+
+# This example dataset assumes that "Country, Age and Salary"
+# are the leading causes of Purchase Rate and has not gone
+# through an optimized process regarding P-values or Adj. R-Squared
+
 # Import the dataset
 dataset = pd.read_csv('Data.csv')
 

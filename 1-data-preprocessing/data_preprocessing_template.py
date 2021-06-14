b1387878d1361857a96df1f44291d66ceeeed22a
mjmeilahn <mjmeilahn@gmail.com>
2021-06-13 17:19:08 -0700

    OK

--- 1-data-preprocessing/data_preprocessing_template.py
+++ 1-data-preprocessing/data_preprocessing_template.py
@@ -1,23 +1,27 @@
-# Data Preprocessing Template
 
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+# Import the dataset
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
+
+# Below code assumes Depedent Variable is in the last column
+
+# Independent Variables "Country, Age, Salary" values as indexes
+x = dataset.iloc[:, :-1].values
+
+# Dependent Variable "Purchased" values as indexes
 y = dataset.iloc[:, 3].values
 
-# Splitting the dataset into the Training set and Test set
+# Split the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+sc_x = StandardScaler()
+x_train = sc_X.fit_transform(x_train)
+x_test = sc_X.transform(x_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
